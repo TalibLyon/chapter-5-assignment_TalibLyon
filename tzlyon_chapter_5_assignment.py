eb1b4daead06755dfe8274a3e75fe2eeb88714de
TalibLyon <tzlyon@aggies.ncat.edu>
2026-02-18 10:05:13 -0500

    Add Loop Mastery Assignment with student details

---
+++ tzlyon_chapter_5_assignment.py
@@ -0,0 +1,10 @@
+# Loop Mastery Assignment
+
+student_name = "Talib Lyon"
+github_username = "TalibLyon"
+date = "February 18, 2026"
+
+print("# Loop Mastery Assignment\n")
+print(f"Student Name: {student_name}")
+print(f"GitHub Username: {github_username}")
+print(f"Date: {date}")
